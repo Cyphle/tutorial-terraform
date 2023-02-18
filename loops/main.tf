3b62535622f0c57411d90c53aedafea9d858a5fd
Cyphle <cyril.phamle@gmail.com>
2023-02-18 18:29:08 +0100

    bla

--- loops/main.tf
+++ loops/main.tf
@@ -24,3 +24,41 @@ resource "aws_iam_user" "example_for_each" {
 output "all_users" {
   value = aws_iam_user.example_for_each
 }
+
+# Loop with for
+output "upper_names" {
+  value = [for name in var.names : upper(name) if length(name) < 5]
+}
+
+variable "hero_thousand_faces" {
+  desription = "map"
+  type       = map(string)
+  default = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "monter"
+  }
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = { for name, role in var.hero_thousand_faces : upper(name) => upper(role) }
+}
+
+# For avec String directive
+variable "names" {
+  description = "Names to render"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
+}
+
+output "for_directive" {
+  value = "%{for name in var.names}${name}, %{endfor}"
+}
+
+output "for_directive_index" {
+  value = "%{for i, name in var.names}${i} ${name},%{endfor}"
+}
